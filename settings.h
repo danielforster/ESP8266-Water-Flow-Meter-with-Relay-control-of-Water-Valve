2db8615756bf93712c9ccfbce634e96b7a0d4e23
danielforster <daniel.forster@keyav.com>
2021-01-23 19:22:53 +0000

    Update settings.h

--- settings.h
+++ settings.h
@@ -3,12 +3,12 @@ const char* ssid = "SSID_HERE";
 const char* password = "PASSWORD_HERE";
 
 // MQTT Cibfiguration
-const char* mqtt_server = "192.168.1.233";
+const char* mqtt_server = "MQTT_SERVER_IP_ADDRESS_HERE";
 const int mqtt_port = 1883;
 const char* mqtt_id = "Water_Flow_Sensor_Utility";
-const char* mqtt_pub_topic_waterflow = "water/utility_flow_sensor/rate";
-const char* mqtt_sub_topic_healthcheck = "water/utility_flow_sensor/heathcheck";
-const char* mqtt_sub_topic_operation = "water/utility_flow_sensor/operation";
+const char* mqtt_pub_topic_waterflow = "water/utility_flow_sensor/rate"; //This is what i use as my topic
+const char* mqtt_sub_topic_healthcheck = "water/utility_flow_sensor/heathcheck"; //This is the healthcheck for the device
+const char* mqtt_sub_topic_operation = "water/utility_flow_sensor/operation"; //This confirms correct operation
 
 // Other params
 const int update_time_sensors = 60000;
